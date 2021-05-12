5c9d577b08fea64fde6d6c0df3013e9eaeeaa1da
Zhao Chen <resiloc@gmail.com>
2021-05-12 07:14:59 -0400

    likes create api

---
+++ likes/api/views.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets, status
+from utils.decorators import required_params
+from likes.api.serializers import LikeSerializer, LikeSerializerForCreate
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class LikeViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = LikeSerializerForCreate
+
+    @required_params(request_attr='data', params=['content_type', 'object_id'])
+    def create(self, request):
+        serializer = LikeSerializerForCreate(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        instance = serializer.save()
+        return Response(
+            LikeSerializer(instance).data,
+            status=status.HTTP_201_CREATED,
+        )
