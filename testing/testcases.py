dcc07473fd9dfe127784e6e853637032a9af1ec0
Zhao Chen <resiloc@gmail.com>
2021-06-17 16:02:38 -0400

    cache with limited size

--- testing/testcases.py
+++ testing/testcases.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.cache import caches
 from django.test import TestCase as DjangoTestCase
+from friendships.models import Friendship
 from likes.models import Like
 from newsfeeds.models import NewsFeed
 from rest_framework.test import APIClient
@@ -29,6 +30,9 @@ class TestCase(DjangoTestCase):
         if password is None:
             password = 'generic password'
         return User.objects.create_user(username, email, password)
+
+    def create_friendship(self, from_user, to_user):
+        return Friendship.objects.create(from_user=from_user, to_user=to_user)
 
     def create_tweet(self, user, content=None):
         if content is None:
